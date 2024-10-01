0844b7b0d3f18b47a74861bb9f0c1caab3f3436f
harsh241082 <145460069+harsh241082@users.noreply.github.com>
2024-10-01 22:02:20 +0530

    completed the functions in line runner

--- src/lineRunner.cpp
+++ src/lineRunner.cpp
@@ -1,7 +1,5 @@
 #include"../include/linerunner.h"
 
-
-
 void func_add(Instruction instruct){
     int rd_index = instruct.rd;
     int rs1 = GetRegister(cpu,instruct.rs1);
@@ -133,7 +131,7 @@ void func_lb(Instruction instruct){
     int rd_index = instruct.rd;
     int rs1 = GetRegister(cpu,instruct.rs1);
     int imm = instruct.immediate;
-    int result = Memory[rs1 + imm];
+    int result = fechMemory(rs1+imm,1);
     result = result & 0x000000FF;
     SetRegister(cpu, rd_index, result);
 }
@@ -141,7 +139,7 @@ void func_lh(Instruction instruct){
     int rd_index = instruct.rd;
     int rs1 = GetRegister(cpu,instruct.rs1);
     int imm = instruct.immediate;
-    int result = Memory[rs1 + imm];
+    int result = fechMemory(rs1+imm,2);
     result = result & 0x0000FFFF;
     SetRegister(cpu, rd_index, result);
 }
@@ -149,7 +147,7 @@ void func_lw(Instruction instruct){
     int rd_index = instruct.rd;
     int rs1 = GetRegister(cpu,instruct.rs1);
     int imm = instruct.immediate;
-    int result = Memory[rs1 + imm];
+    int result = fechMemory(rs1+imm,4);
     result = result & 0xFFFFFFFF;
     SetRegister(cpu, rd_index, result);
 }
@@ -157,14 +155,14 @@ void func_ld(Instruction instruct){
     int rd_index = instruct.rd;
     int rs1 = GetRegister(cpu,instruct.rs1);
     int imm = instruct.immediate;
-    int result = Memory[rs1 + imm];
+    int result = fechMemory(rs1+imm,8);
     SetRegister(cpu, rd_index, result);
 }
 void func_lbu(Instruction instruct){
     int rd_index = instruct.rd;
     int rs1 = GetRegister(cpu,instruct.rs1);
     int imm = instruct.immediate;
-    unsigned int result = Memory[rs1 + imm];
+    unsigned int result =fechMemory(rs1+imm,1);
     result = result & 0x000000FF;
     SetRegister(cpu, rd_index, result);
 }
@@ -172,7 +170,7 @@ void func_lhu(Instruction instruct){
     int rd_index = instruct.rd;
     int rs1 = GetRegister(cpu,instruct.rs1);
     int imm = instruct.immediate;
-    unsigned int result = Memory[rs1 + imm];
+    unsigned int result = fechMemory(rs1+imm,2);
     result = result & 0x0000FFFF;
     SetRegister(cpu, rd_index, result);
 }
@@ -180,32 +178,32 @@ void func_lwu(Instruction instruct){
     int rd_index = instruct.rd;
     int rs1 = GetRegister(cpu,instruct.rs1);
     int imm = instruct.immediate;
-    unsigned int result = Memory[rs1 + imm];
+    unsigned int result = fechMemory(rs1+imm,4 );
     SetRegister(cpu, rd_index, result);
 }
 void func_sb(Instruction instruct){
     int rs1 = GetRegister(cpu,instruct.rs1);
     int rs2 = GetRegister(cpu,instruct.rs2);
     int imm = instruct.immediate;
-    Memory[rs1 + imm] = rs2;
+    storeMemory(rs1+imm,1,rs2);
 }
 void func_sh(Instruction instruct){
     int rs1 = GetRegister(cpu,instruct.rs1);
     int rs2 = GetRegister(cpu,instruct.rs2);
     int imm = instruct.immediate;
-    Memory[rs1 + imm] = rs2;
+    storeMemory(rs1+imm,2,rs2);
 }
 void func_sw(Instruction instruct){
     int rs1 = GetRegister(cpu,instruct.rs1);
     int rs2 = GetRegister(cpu,instruct.rs2);
     int imm = instruct.immediate;
-    Memory[rs1 + imm] = rs2;
+    storeMemory(rs1+imm,4,rs2);
 }
 void func_sd(Instruction instruct){
     int rs1 = GetRegister(cpu,instruct.rs1);
     int rs2 = GetRegister(cpu,instruct.rs2);
     int imm = instruct.immediate;
-    Memory[rs1 + imm] = rs2;
+    storeMemory(rs1+imm,8,rs2);
 }
 void func_beq(Instruction instruct){
     int rs1 = GetRegister(cpu,instruct.rs1);
@@ -218,7 +216,7 @@ void func_beq(Instruction instruct){
 void func_bne(Instruction instruct){
     int rs1 = GetRegister(cpu,instruct.rs1);
     int rs2 = GetRegister(cpu,instruct.rs2);
-    int imm = instruct.immediate;
+    int imm = instruct.immediate/4;
     if(rs1 != rs2){
         ProgramCounter += imm;
     }
@@ -226,7 +224,7 @@ void func_bne(Instruction instruct){
 void func_blt(Instruction instruct){
     int rs1 = GetRegister(cpu,instruct.rs1);
     int rs2 = GetRegister(cpu,instruct.rs2);
-    int imm = instruct.immediate;
+    int imm = instruct.immediate/4;
     if(rs1 < rs2){
         ProgramCounter += imm;
     }
@@ -234,7 +232,7 @@ void func_blt(Instruction instruct){
 void func_bge(Instruction instruct){
     int rs1 = GetRegister(cpu,instruct.rs1);
     int rs2 = GetRegister(cpu,instruct.rs2);
-    int imm = instruct.immediate;
+    int imm = instruct.immediate/4;
     if(rs1 >= rs2){
         ProgramCounter += imm;
     }
@@ -242,7 +240,7 @@ void func_bge(Instruction instruct){
 void func_bltu(Instruction instruct){
     unsigned int rs1 = GetRegister(cpu,instruct.rs1);
     unsigned int rs2 = GetRegister(cpu,instruct.rs2);
-    int imm = instruct.immediate;
+    int imm = instruct.immediate/4;
     if(rs1 < rs2){
         ProgramCounter += imm;
     }
@@ -250,7 +248,7 @@ void func_bltu(Instruction instruct){
 void func_bgeu(Instruction instruct){
     unsigned int rs1 = GetRegister(cpu,instruct.rs1);
     unsigned int rs2 = GetRegister(cpu,instruct.rs2);
-    int imm = instruct.immediate;
+    int imm = instruct.immediate/4;
     if(rs1 >= rs2){
         ProgramCounter += imm;
     }
@@ -258,10 +256,25 @@ void func_bgeu(Instruction instruct){
 void func_jal(Instruction instruct){
     int rd_index = instruct.rd;
     SetRegister(cpu, rd_index, (ProgramCounter+1)*4);
-    int imm = instruct.immediate;
+    int imm = instruct.immediate/4;
     ProgramCounter += imm;
     stackPointer--;
 }
-void func_jalr(Instruction instruct){}
-void func_lui(Instruction instruct){}
-void func_auipc(Instruction instruct){}
+void func_jalr(Instruction instruct){
+    int rd_index = instruct.rd;
+    int rs1 = GetRegister(cpu,instruct.rs1);
+    SetRegister(cpu, rd_index, (ProgramCounter+1)*4);
+    int imm = instruct.immediate/4;
+    ProgramCounter = rs1 + imm;
+    stackPointer++;
+}
+void func_lui(Instruction instruct){
+    int rd_index = instruct.rd;
+    int imm = instruct.immediate;
+    SetRegister(cpu, rd_index, imm << 12);
+}
+void func_auipc(Instruction instruct){
+    int rd_index = instruct.rd;
+    int imm = instruct.immediate;
+    SetRegister(cpu, rd_index, ProgramCounter*4 + imm);
+}
